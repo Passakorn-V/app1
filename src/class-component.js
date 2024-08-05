41472efbb965ae8c1868ba55bd43408f289d4184
Passakorn-V <156067673+Passakorn-V@users.noreply.github.com>
2024-08-05 12:00:25 +0700

    Add Calendar

---
+++ src/class-component.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default class Calendar extends React.Component{
+    getDate(){
+        const dayNames=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
+        const monthNames=['January','February','March','April','May','June','July','August','September','October','November','December']
+        const date = new Date()
+        const weekDay = dayNames[date.getDay()]
+        const day = date.getDay()
+        const month = monthNames[date.getMonth()]
+        const year = date.getFullYear()
+        return `${weekDay} ${day} ${month} ${year}`
+    }
+    render() {
+        return <p><center>{this.getDate()}</center></p>
+    }
+}
