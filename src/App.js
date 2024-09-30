0f9a88f4a8a18b35ff48294e53be8c5e2e84fd41
Passakorn-V <156067673+Passakorn-V@users.noreply.github.com>
2024-09-30 11:16:03 +0700

    Add Form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import Router3 from './router3'
-
-
+// import FormBootstrap from './form-Bootstrap'
+// import FormText from'./form-text';
+import FormSelectFile from'./form-select-file';
 
 export default function App(){
-  return <Router3/>
+  return <FormSelectFile />
 }
